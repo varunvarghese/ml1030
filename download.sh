6f16cdf541f7599f03f3a7e8bd01680051ef430d
varunvarghese <varunmuriyanat@gmail.com>
2019-03-16 14:20:44 -0400

    changes

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+wget http://data.insideairbnb.com/canada/on/toronto/2019-03-07/data/listings.csv.gz
+wget http://data.insideairbnb.com/canada/on/toronto/2019-03-07/data/calendar.csv.gz
+wget http://data.insideairbnb.com/canada/on/toronto/2019-03-07/data/reviews.csv.gz
+wget http://data.insideairbnb.com/canada/on/toronto/2019-03-07/visualisations/listings.csv
+wget http://data.insideairbnb.com/canada/on/toronto/2019-03-07/visualisations/reviews.csv
+wget http://data.insideairbnb.com/canada/on/toronto/2019-03-07/visualisations/neighbourhoods.csv
+wget http://data.insideairbnb.com/canada/on/toronto/2019-03-07/visualisations/neighbourhoods.geojson
+
+
